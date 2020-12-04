272f07a7616b347bb2c07bd98b1fbfc3d103a61f
Magnus Gasslander <magnus.gasslander@gmail.com>
2020-12-04 22:54:10 +0100

    day 3

---
+++ 2020/03/first.scala
@@ -0,0 +1,9 @@
+val rows = io.Source.stdin.getLines.toList
+
+val width = rows(0).length
+
+val tmp = for (row <- rows.zip(0 to rows.length)
+               if (row._1.toList((row._2 * 3) % width ) == '#')
+               ) yield 1
+
+println (tmp.length)
